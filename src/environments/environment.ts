8a12596879284e5461c28a4fd4721257a5e81172
henlik <likoyo.henock.lh@gmail.com>
2021-12-23 21:28:21 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-  apiKey: "AIzaSyBoE4IozC_dsI_DNYEr50aie2QvTbogG7Q",
-  authDomain: "ume-functions.firebaseapp.com",
-  projectId: "ume-functions",
-  storageBucket: "ume-functions.appspot.com",
-  messagingSenderId: "1002216754764",
-  appId: "1:1002216754764:web:c4b55a63da627d1baa6ee9",
-  measurementId: "G-Q9ENG1M922"
+  
   },
 };
 
